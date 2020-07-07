e818d9aa15f32a7b7f71b75569e6d545114e4ecc
Ryan Jacobo <ryanjacobo@yahoo.com>
2020-07-07 15:30:53 -0700

    Added welcome greeting upon login
    Added welcome greeting upon login

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -53,6 +53,30 @@ export const register = ({ username, email, password }) => dispatch => {
     })
 };
 
+// Login User
+export const  login = ({ username, password }) => dispatch => {
+    // Headers
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    // Request body
+    const body = JSON.stringify({username, password});
+    axios.post('/api/auth', body, config)
+    .then(res => dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+    }))
+    .catch(err => {
+        dispatch(returnErrors(err.response.data, err.response.status, 'LOGIN_FAIL'));
+        dispatch({
+            type: LOGIN_FAIL
+        })
+    })
+};
+
 // Logout User
 export const logout = () => {
     return {
@@ -74,7 +98,7 @@ export const tokenConfig = getState => {
 
     // If token, add to headers
     if (token) {
-        config.headers['x-auth-toke'] = token;
+        config.headers['x-auth-token'] = token;
     }
 
     return config;
