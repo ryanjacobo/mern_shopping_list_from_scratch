360049c292b76c7f470c3a19ee1c23f51b86d97b
Ryan Jacobo <ryanjacobo@yahoo.com>
2020-07-07 18:29:03 -0700

    Applied authorization for delete function
    Applied authorization for delete function

--- client/src/components/itemModal.js
+++ client/src/components/itemModal.js
@@ -11,12 +11,16 @@ import {
 } from 'reactstrap';
 import { connect } from 'react-redux';
 import { addItem } from '../actions/itemActions';
-// import { v4 as uuid } from 'uuid';
+import PropTypes from 'prop-types';
 
 class ItemModal extends Component {
     state = {
         modal: false,
         name: ''
+    }
+
+    static propTypes = {
+        isAuthenticated: PropTypes.bool
     }
 
     toggle = () => {
@@ -47,12 +51,15 @@ class ItemModal extends Component {
     render() {
         return(
             <div>
-                <Button
-                color="dark"
-                style={{marginBottom: '2rem'}}
-                onClick={this.toggle}
-                >Add Item</Button>
-
+                {this.props.isAuthenticated ? (
+                     <Button
+                     color="dark"
+                     style={{marginBottom: '2rem'}}
+                     onClick={this.toggle}
+                     >Add Item</Button>
+                ) : (<h4 className='mb-3 ml-4'>Please log in to manage items</h4>)
+                }
+               
                 <Modal 
                 isOpen={this.state.modal}
                 toggle={this.toggle}
@@ -84,7 +91,8 @@ class ItemModal extends Component {
 }
 
 const mapStateToProps = state => ({
-    item: state.item
+    item: state.item,
+    isAuthenticated: state.auth.isAuthenticated
 });
 
 export default connect(mapStateToProps, { addItem })(ItemModal);
